fe8871b585dd846ddd97e69a85755d8ff9524a44
omega-Lee <2033211717@qq.com>
2020-05-01 21:22:14 +0800

    更新计时部分

--- Rx.c
+++ Rx.c
@@ -2,6 +2,7 @@
 #include "oled.h"
 #include "bmp.h"
 #include "24L01.h"
+#include "ds1302.h"
 
 #define uchar unsigned char //宏定义无符号字符型
 #define uint unsigned int	//宏定义无符号整型
@@ -17,6 +18,8 @@ unsigned char tf = 0;
 unsigned char TxBuf[10] = {0};
 unsigned char RxBuf[10] = {0};
 
+uchar DisplayData[12];
+
 void delay() //延时程序
 {
 	uchar m, n, s;
@@ -24,6 +27,29 @@ void delay() //延时程序
 		for (n = 20; n > 0; n--)
 			for (s = 248; s > 0; s--)
 				;
+}
+
+void datapros()
+{
+	Ds1302ReadTime(); //读取时间
+
+	DisplayData[11] = TIME[6] >> 4;	  //年   /16==>>4（右移动4位）
+	DisplayData[10] = TIME[6] & 0x0f; //0000 1111 取低位
+
+	DisplayData[9] = TIME[4] / 16;	 //月
+	DisplayData[8] = TIME[4] & 0x0f; //0000 1111 取低位
+
+	DisplayData[7] = TIME[3] / 16;	 //日
+	DisplayData[6] = TIME[3] & 0x0f; //0000 1111 取低位
+
+	DisplayData[5] = TIME[2] >> 4;	 //时   /16==>>4（右移动4位）
+	DisplayData[4] = TIME[2] & 0x0f; //0000 1111 取低位
+
+	DisplayData[3] = TIME[1] / 16;	 //分
+	DisplayData[2] = TIME[1] & 0x0f; //0000 1111 取低位
+
+	DisplayData[1] = TIME[0] / 16;	 //分
+	DisplayData[0] = TIME[0] & 0x0f; //0000 1111 取低位
 }
 
 int main(void)
@@ -50,11 +76,37 @@ int main(void)
 	TxBuf[4] = 0x00;
 
 	OLED_ShowString(0, 0, "Receive", 16);
-	OLED_ShowString(0, 2, "Time:", 16);
+	//OLED_ShowString(0, 2, "20/05/01/12/30", 16);
+
+	Ds1302Init();
 
 	while (1)
 	{
+		datapros();
+
 		OLED_ShowString(0, 0, "Receive", 16);
+
+		OLED_ShowNum(0, 2, DisplayData[11], 1, 16); //年
+		OLED_ShowNum(8, 2, DisplayData[10], 1, 16);
+		OLED_ShowString(16, 2, "/", 16);
+
+		OLED_ShowNum(24, 2, DisplayData[9], 1, 16); //月
+		OLED_ShowNum(32, 2, DisplayData[8], 1, 16);
+
+		OLED_ShowNum(40, 2, DisplayData[7], 1, 16); //日
+		OLED_ShowNum(48, 2, DisplayData[6], 1, 16);
+		OLED_ShowString(56, 2, "/", 16);
+
+		OLED_ShowNum(64, 2, DisplayData[5], 1, 16); //时
+		OLED_ShowNum(72, 2, DisplayData[4], 1, 16);
+		OLED_ShowString(80, 2, "/", 16);
+
+		OLED_ShowNum(88, 2, DisplayData[3], 1, 16); //分
+		OLED_ShowNum(96, 2, DisplayData[2], 1, 16);
+		OLED_ShowString(104, 2, "/", 16);
+
+		OLED_ShowNum(112, 2, DisplayData[1], 1, 16); //秒
+		OLED_ShowNum(120, 2, DisplayData[0], 1, 16);
 
 		//按键发送测试
 		if (key2 == 0)
